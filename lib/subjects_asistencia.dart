0a3ce440f5746ca9e7ca53d91b039332b9802b7d
Dworak <esteban.cervantes@resser.com>
2020-05-05 16:38:28 -0500

    asistencias student

--- lib/subjects_asistencia.dart
+++ lib/subjects_asistencia.dart
@@ -11,8 +11,7 @@ class SubjectAsistencia extends StatefulWidget {
   // final String email;
   final GoogleSignInAccount account;
 
-  const SubjectAsistencia({Key key, @required this.account})
-      : super(key: key);
+  const SubjectAsistencia({Key key, @required this.account}) : super(key: key);
 
   @override
   _SubjectAsistenciaState createState() => _SubjectAsistenciaState();
@@ -24,23 +23,18 @@ class _SubjectAsistenciaState extends State<SubjectAsistencia> {
   @override
   void initState() {
     super.initState();
-    Firestore.instance.collection('subjects').snapshots()
-    .listen((data) {
+    Firestore.instance.collection('subjects').snapshots().listen((data) {
       List<Subject> subjects = new List<Subject>();
       data.documents.forEach((doc) {
         print(doc);
-        
+
         int a = 0;
-        if(doc["students"]!=null){
-          for (var i = 0;
-                                    i < doc["students"].length;
-                                    i++) {
-                                  if (doc["students"][i]["email"] ==
-                                      widget.account.email) {
-                                    a =doc["students"][i]["assist"];
-                                  }
-                                  
-                                }
+        if (doc["students"] != null) {
+          for (var i = 0; i < doc["students"].length; i++) {
+            if (doc["students"][i]["email"] == widget.account.email) {
+              a = doc["students"][i]["assist"];
+            }
+          }
         }
 
         Subject subject = new Subject(
@@ -66,10 +60,37 @@ class _SubjectAsistenciaState extends State<SubjectAsistencia> {
       builder: (BuildContext context) {
         // return object of type Dialog
         return AlertDialog(
-          title: new Text("Imagen guardada correctamente"),
+          title: new Text("¡Asistencia tomada correctamente!"),
           content: new Icon(
             Icons.check,
             color: Colors.green,
+          ),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Cerrar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+// user defined function
+  void _showDialogError() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("¡Código incorrecto!"),
+          content: new Icon(
+            Icons.cancel,
+            color: Colors.red,
           ),
           actions: <Widget>[
             // usually buttons at the bottom of the dialog
@@ -105,58 +126,74 @@ class _SubjectAsistenciaState extends State<SubjectAsistencia> {
                         title: Text(_subjects[index].name +
                             " - " +
                             _subjects[index].schedule),
-                        subtitle: Text("Asistencias: " + _subjects[index].assist.toString()),
+                        subtitle: Text("Asistencias: " +
+                            _subjects[index].assist.toString()),
                         trailing: GestureDetector(
                           onTap: () async {
-                            // final String currentTeam =
-                            Firestore.instance
-                                .collection('subjects')
-                                .document(_subjects[index].id)
-                                .get()
-                                .then((DocumentSnapshot ds) {
-                              // use ds as a snapshot
-                              print(ds);
+                            final String code =
+                                await _asyncInputDialog(context);
+                            if (code != null && code != "") {
+                              if (code == _subjects[index].code) {
+                                Firestore.instance
+                                    .collection('subjects')
+                                    .document(_subjects[index].id)
+                                    .get()
+                                    .then((DocumentSnapshot ds) {
+                                  // use ds as a snapshot
+                                  print(ds);
 
-                              var newStudents = <String, dynamic>{"list": []};
-                              bool f = false;
-                              if (ds.data["students"] != null) {
-                                for (var i = 0;
-                                    i < ds.data["students"].length;
-                                    i++) {
-                                  if (ds.data["students"][i]["email"] ==
-                                      widget.account.email) {
-                                    f = true;
+                                  var newStudents = <String, dynamic>{
+                                    "list": []
+                                  };
+
+                                  if (ds.data["students"] != null) {
+                                    for (var i = 0;
+                                        i < ds.data["students"].length;
+                                        i++) {
+                                      if (ds.data["students"][i]["email"] ==
+                                          widget.account.email) {
+                                        newStudents["list"].add({
+                                          "email": ds.data["students"][i]
+                                              ["email"],
+                                          "assist": ds.data["students"][i]
+                                                  ["assist"] +
+                                              1
+                                        });
+                                      }else{
+
+                                      newStudents["list"].add({
+                                        "email": ds.data["students"][i]
+                                            ["email"],
+                                        "assist": ds.data["students"][i]
+                                            ["assist"]
+                                      });
+                                      }
+                                    }
                                   }
-                                  newStudents["list"].add({
-                                    "email": ds.data["students"][i]["email"],
-                                    "assist": ds.data["students"][i]["assist"]
+
+                                  final DocumentReference postRef =
+                                      Firestore.instance.document(
+                                          'subjects/' + _subjects[index].id);
+
+                                  Firestore.instance
+                                      .runTransaction((Transaction tx) async {
+                                    DocumentSnapshot postSnapshot =
+                                        await tx.get(postRef);
+                                    if (postSnapshot.exists) {
+                                      await tx.update(
+                                          postRef, <String, dynamic>{
+                                        'students': newStudents["list"]
+                                      });
+                                    }
                                   });
-                                }
-                              }
-                              if (f == false) {
-                                newStudents["list"].add({
-                                  "email": widget.account.email,
-                                  "assist": 0
+
+                                  _showDialog();
+                                  Navigator.of(context).pop(null);
                                 });
+                              } else {
+                                _showDialogError();
                               }
-
-                              final DocumentReference postRef = Firestore
-                                  .instance
-                                  .document('subjects/' + _subjects[index].id);
-
-                              Firestore.instance
-                                  .runTransaction((Transaction tx) async {
-                                DocumentSnapshot postSnapshot =
-                                    await tx.get(postRef);
-                                if (postSnapshot.exists) {
-                                  await tx.update(postRef, <String, dynamic>{
-                                    'students': newStudents["list"]
-                                  });
-                                }
-                              });
-
-                              Navigator.of(context).pop(null);
-                            });
+                            }
                           },
                           child: Icon(Icons.list),
                         )),
@@ -177,14 +214,14 @@ Future<String> _asyncInputDialog(BuildContext context) async {
         false, // dialog is dismissible with a tap on the barrier
     builder: (BuildContext context) {
       return AlertDialog(
-        title: Text('Editar código de Asistencia'),
+        title: Text('Ingresar código de Asistencia'),
         content: new Row(
           children: <Widget>[
             new Expanded(
                 child: new TextField(
               autofocus: true,
               decoration: new InputDecoration(
-                  labelText: 'Código de asistencia', hintText: '123456'),
+                  labelText: 'Código de asistencia', hintText: 'XXXXXX'),
               onChanged: (value) {
                 teamName = value;
               },
